771a0863141745790af7afa5621e897680b6e01d
Nataly Blum <nblyumenshteyn@gmail.com>
2021-04-15 18:11:55 +0400

    test fp

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,30 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        sum = 0
+        cnt = 0
+        array.map do |val|
+          next unless !val['country'].nil? && (val['country'].split(',').length > 1 &&
+            !val['rating_kinopoisk'].nil? &&
+            val['rating_kinopoisk'].to_f.positive?)
+
+          sum += val['rating_kinopoisk'].to_f
+          cnt += 1
+        end
+
+        sum / cnt
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        films.reduce(0) do |sum, val|
+          if !val['name'].nil? && !val['rating_kinopoisk'].nil? &&
+             val['rating_kinopoisk'].to_f >= threshold
+            sum + val['name'].count('и')
+          else
+            sum
+          end
+        end
       end
     end
   end
